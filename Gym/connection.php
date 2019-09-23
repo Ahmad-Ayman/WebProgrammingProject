6d234ec0eb2b36878a773e1142609677b6d75ce5
Ahmed Ayman Abd El-Moneim <33450003+Ahmeda1708@users.noreply.github.com>
2019-09-24 01:51:44 +0200

    Update connection.php

--- Gym/connection.php
+++ Gym/connection.php
@@ -1,10 +1,10 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-$servername = "213.190.6.127";
-$username = "u972902333_wp";
-$password = "webprogramming";
-$dbname = "u972902333_gym";
+$servername = "localhost";
+$username = "dbusername";
+$password = "dbuserpassowrd";
+$dbname = "dbname";
 // Create connection
 $connection = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
